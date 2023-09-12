9404e52272e1061e152d686ad4dd683c1fd19498
ViktorBelevv <victorbelev@abv.bg>
2023-09-12 23:38:16 +0300

    Added design

--- ecom/products/forms.py
+++ ecom/products/forms.py
@@ -1,9 +1,10 @@
 from django import forms
 
+from ecom.core.boostrap_mixins import BoostrapFormMixin
 from ecom.products.models import Product
 
 
-class ProductForm(forms.ModelForm):
+class ProductForm(BoostrapFormMixin, forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
