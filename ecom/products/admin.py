4f5704b5b71a407487338299272aa5057066676e
ViktorBelevv <victorbelev@abv.bg>
2023-09-22 00:05:42 +0300

    Edited admin

--- ecom/products/admin.py
+++ ecom/products/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 
-from ecom.products.models import Product
+from ecom.products.models import Product, Review
 
 
 class ProductsAdmin(admin.ModelAdmin):
     list_display = ('name', 'price')
 
 
+class ReviewsAdmin(admin.ModelAdmin):
+    list_display = ('product', 'rating')
+
+
 admin.site.register(Product, ProductsAdmin)
+admin.site.register(Review, ReviewsAdmin)
