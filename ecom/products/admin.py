9d8a9aaa868ccb0599608f60e095d045a143562e
ViktorBelevv <victorbelev@abv.bg>
2023-09-22 00:00:40 +0300

    Added admin

--- ecom/products/admin.py
+++ ecom/products/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from ecom.products.models import Product
+
+
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price')
+
+
+admin.site.register(Product, ProductsAdmin)
